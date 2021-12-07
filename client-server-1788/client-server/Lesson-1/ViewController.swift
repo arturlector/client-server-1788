9cd39b43fb475d797c0bee17402b3e021a09e9a9
arturlector <art.igberdin@gmail.com>
2021-12-07 20:35:34 +0300

    Initialize project

---
+++ client-server-1788/client-server/Lesson-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-server-1788
+//
+//  Created by Artur Igberdin on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
