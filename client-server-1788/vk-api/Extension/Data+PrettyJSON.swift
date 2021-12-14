cb17587dbb2c1a255b00b046a24a61f1809b6af0
arturlector <art.igberdin@gmail.com>
2021-12-14 21:45:21 +0300

    Get friends reguest, 3 methods of parsing data to json

---
+++ client-server-1788/vk-api/Extension/Data+PrettyJSON.swift
@@ -0,0 +1,19 @@
+//
+//  Data+PrettyJSON.swift
+//  client-server-1788
+//
+//  Created by Artur Igberdin on 14.12.2021.
+//
+
+import Foundation
+
+extension Data {
+    
+    var prettyJSON: NSString? { /// NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
